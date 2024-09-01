dd09b11936efbefa67be950c0203db4e995c9404
KillPhill2111 <filipe.fonseca.eng@outlook.com>
2024-09-01 15:51:45 -0300

    'quarto exercicio'

---
+++ Questão 04/Questão04/Questão04/Entities/Faturamento.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Questão04.Entities;
+using System.Globalization;
+
+namespace Questão04.Entities
+{
+    class Faturamento
+    {
+        public string Estado { get; set; }
+        public double ValorFaturamento { get; set; }
+
+
+        public Faturamento(string estado, double valorFaturamento) //cosntrutor simples 
+        {   
+            Estado = estado;
+            ValorFaturamento= valorFaturamento;
+        }
+
+        public static Faturamento TransformaParaStrings(string linha)
+        {
+          
+            linha = linha.Trim(); // Remove espaços em branco no início e no final
+
+            // Verificar se a linha contém o caractere '–'
+            //if (!linha.Contains('–'))
+            //{
+            //    throw new ArgumentException("Formato de linha inválido: falta caractere separador.");
+            //}
+
+            var partes = linha.Split('–'); // Dividir a linha em estado e valor
+
+            // Verificar se o número de partes está correto
+            if (partes.Length != 2)
+            {
+                throw new ArgumentException("Formato de linha inválido: deve conter exatamente um caractere separador.");
+            }
+
+            string estado = partes[0].Trim(); // Guarda o estado
+            double valor;
+
+            // Tentar converter o valor e capturar possíveis erros
+            if (!double.TryParse(partes[1].Trim(), NumberStyles.Number, CultureInfo.InvariantCulture, out valor))
+            {
+                throw new ArgumentException("Formato de valor inválido.");
+            }
+
+            return new Faturamento(estado, valor); // Cria um novo objeto Faturamento
+        }
+    }
+
+    
+}
---
+++ Questão 04/Questão04/Questão04/Program.cs
@@ -0,0 +1,41 @@
+﻿using Questão04.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questão04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string caminhoTxt = @"C:\Users\filip\Documents\Filipe\Projetos de desenvolvimento e programação\Target- prova tecnica- São Paulo\Targe--prova-tecnica--S-o-Paulo\Questão 04\Questão04\Questão04\Entities\FaturamentoEstado.txt";
+
+            var faturamentos=new List<Faturamento>();//guardamso a lista numa varivel var
+
+            foreach (string linhaArq in File.ReadLines(caminhoTxt)) //buscamos o arquivo, neste exrcicio estamos usando um xt
+            {
+                Faturamento fat = Faturamento.TransformaParaStrings(linhaArq);
+                faturamentos.Add(fat);
+            }
+
+            //fazer a soma dos faturamentos totais:
+            double faturamentoTotal = 0;
+            foreach (var fat in faturamentos)
+            {
+                faturamentoTotal += fat.ValorFaturamento;
+            }
+            Console.WriteLine("Porcentagem de contribuição do total por estado:");
+            foreach (var fat in faturamentos)
+            {
+                double porcentagem = (fat.ValorFaturamento / faturamentoTotal) * 100;
+                Console.WriteLine($"{fat.Estado}: {porcentagem:F2}%");
+            }
+
+        }
+    }
+}
