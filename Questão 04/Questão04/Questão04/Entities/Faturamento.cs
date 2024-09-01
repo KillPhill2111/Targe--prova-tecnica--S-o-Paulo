c66018932bf3cbca86ad194b45e3424556c94516
KillPhill2111 <filipe.fonseca.eng@outlook.com>
2024-09-01 16:13:26 -0300

    'correção exercicio 4'

--- Questão 04/Questão04/Questão04/Entities/Faturamento.cs
+++ Questão 04/Questão04/Questão04/Entities/Faturamento.cs
@@ -24,33 +24,20 @@ namespace Questão04.Entities
 
         public static Faturamento TransformaParaStrings(string linha)
         {
-          
-            linha = linha.Trim(); // Remove espaços em branco no início e no final
-
-            // Verificar se a linha contém o caractere '–'
-            //if (!linha.Contains('–'))
-            //{
-            //    throw new ArgumentException("Formato de linha inválido: falta caractere separador.");
-            //}
-
-            var partes = linha.Split('–'); // Dividir a linha em estado e valor
-
-            // Verificar se o número de partes está correto
+            if (string.IsNullOrWhiteSpace(linha))
+            {
+                throw new ArgumentException("A linha não pode estar vazia ou conter apenas espaços.");
+            }
+            var partes = linha.Split(':');
             if (partes.Length != 2)
             {
-                throw new ArgumentException("Formato de linha inválido: deve conter exatamente um caractere separador.");
+                throw new ArgumentException("Formato de linha inválido: deve conter exatamente um caractere separador ':'.");
             }
+            string estado = partes[0].Trim();
+            string valorString = partes[1].Trim();
+            double valor=double.Parse(valorString);
 
-            string estado = partes[0].Trim(); // Guarda o estado
-            double valor;
-
-            // Tentar converter o valor e capturar possíveis erros
-            if (!double.TryParse(partes[1].Trim(), NumberStyles.Number, CultureInfo.InvariantCulture, out valor))
-            {
-                throw new ArgumentException("Formato de valor inválido.");
-            }
-
-            return new Faturamento(estado, valor); // Cria um novo objeto Faturamento
+            return new Faturamento(estado, valor);
         }
     }
 
